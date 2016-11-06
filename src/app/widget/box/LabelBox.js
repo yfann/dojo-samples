24f397a3d9d3063dc4f29b3c42daa0b86ffd9b73
Fan <fan.yu@philips.com>
2016-11-06 22:53:55 +0800

    update

--- src/app/widget/box/LabelBox.js
+++ src/app/widget/box/LabelBox.js
@@ -2,15 +2,25 @@ define([
     'dijit/_WidgetBase',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
+    'dojo/_base/lang',
+    'dojo/on',
     'dojo/_base/declare',
     'dojo/text!./templates/label-box.html'
-],function(_WidgetBase,_TemplatedMixin,_WidgetsInTemplateMixin,declare,template){
+],function(_WidgetBase,_TemplatedMixin,_WidgetsInTemplateMixin,lang,on,declare,template){
     return declare('app.weidget.LabelBox',[
         _WidgetBase,
         _TemplatedMixin,
         _WidgetsInTemplateMixin
     ],{
         templateString:template,
-        constructor:function(){}
+        postCreate:function(){
+            // on(this.textBoxNode,'change',lang.hitch(this,function(){
+            //     console.log(this.textBoxNode.get('value'));
+            // }));
+
+            // this.textBoxNode.watch('value',lang.hitch(this,function(){
+            //     console.log(this.textBoxNode.get('value'));
+            // }));
+        }
     })
 });
