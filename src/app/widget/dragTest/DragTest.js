c53fdb33cdc2f843bffe4c191bfd7de55aa7f7de
Fan <fan.yu@philips.com>
2017-09-29 15:33:37 +0800

    update drag event

--- src/app/widget/dragTest/DragTest.js
+++ src/app/widget/dragTest/DragTest.js
@@ -2,11 +2,13 @@ define([
     'dijit/_WidgetBase',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
+    "dojo/dnd/Moveable",
+    'dojo/on',
     'dojo/_base/declare',
     'dojo/_base/lang',
     'dojo/text!./templates/drag-test.html',
     'dojox/mdnd/adapter/DndFromDojo'
-],function(_WidgetBase,_TemplatedMixin,_WidgetsInTemplateMixin,declare,lang,template,DndFromDojo){
+],function(_WidgetBase,_TemplatedMixin,_WidgetsInTemplateMixin,Moveable,on,declare,lang,template,DndFromDojo){
     return declare('app.weidget.DragTest',[
         _WidgetBase,
         _TemplatedMixin,
@@ -20,6 +22,26 @@ define([
             // this.subscribe('/dnd/drop/after',function(){
             //     console.log('drag');
             // });
+            on(this.mouseNode,'mousemove',function(){
+                console.log('mouse move');
+            });
+           on(this.mouseNode,'drag',function(){
+                console.log('drag');
+            });
+
+            var el=document.getElementById("mm");
+            el.addEventListener("mousemove",function(){
+                console.log("mm move");
+            },false);
+
+             el.addEventListener("drag",function(){
+                console.log("mm drag");
+            },false);
+
+            document.addEventListener("drag",function(){
+                console.log("document drag");
+                debugger
+            },false);
         },
         _updateAreas:function(){
                 // var _areaManager = dojox.mdnd.areaManager();
